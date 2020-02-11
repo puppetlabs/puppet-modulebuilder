480122e55c6532bb579a554b8895a4a1f0864b17
sheena <sheena@puppet.com>
2020-02-11 13:54:51 +0000

    Initial commit of rspec_honeycomb_formatter gem scaffolding

---
+++ lib/puppet/modulebuilder.rb
@@ -0,0 +1,8 @@
+require "puppet/modulebuilder/version"
+
+module Puppet
+  module Modulebuilder
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/puppet/modulebuilder/version.rb
@@ -0,0 +1,5 @@
+module Puppet
+  module Modulebuilder
+    VERSION = "0.1.0"
+  end
+end
